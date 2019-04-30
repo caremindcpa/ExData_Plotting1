7ec9fd5b0bc0b00d8f5664d9d3626550ace07449
Chunna Mao <48506278+caremindcpa@users.noreply.github.com>
2019-04-29 23:18:01 -0700

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,4 +1,3 @@
-
 ## Read the data and subset the two days of "2007-02-01","2007-02-02"
 library(data.table)
 library(lubridate)
@@ -6,19 +5,24 @@ epcall <- fread("household_power_consumption.txt",stringsAsFactors = F)
 epc <- subset(epcall, dmy(epcall$Date)=="2007-02-01"|dmy(epcall$Date)=="2007-02-02")
 rm(epcall)
 
-##===== Plot1: Draw histogram plot of plot1. picture saved in the file plot1.png
+##===== Plot2: Draw line graph of plot2. picture saved in the file plot2.png
 
 ## set the device of png. keep the defult png size set, as it is 480 pixels~480 pixels
 ## set background as transparent
-png("plot1.png")
+png("plot2.png")
 par(bg="transparent")
+
+## get the x-axis variable of "time".
+time <- dmy_hms(paste(epc$Date,epc$Time))
 
 ## get the y-axis variables, by change the Global_active_power value to numbers.
 gap <- as.numeric(epc$Global_active_power)
 
-## Draw the histogram with xlab and main title.
+## Draw the plot with ylab and no xlab.
 par(bg="transparent")
-hist(gap,col="red",
-     xlab="Global Active Power (kilowatts)",main="Global Active Power")
+plot(time,gap,type="l",ylab="Global Active Power (kilowatts)",xlab="")
+
 ## close the device.
 dev.off()
+
+
